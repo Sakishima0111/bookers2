0f582c32e8b5b33804c5c0380b80cf5e61424908
EC2 Default User <ec2-user@ip-172-31-1-195.ap-northeast-1.compute.internal>
2024-01-17 07:21:11 +0000

    4thcommit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to users_path
+  #後で↑リダイレクト先修正↓追加
   end
 
   def show
+    @books = Book.all
   end
-  
-  def create
-  end
+
 
   def edit
   end
-  
+
   def destroy
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def edit
   end
 
   def show
+        
   end
 
   def create
